0a0594271da6d4de5f38ee13e19622fbea291694
Ashish Bhoya <ashishbhoya@Ashishs-MacBook-Air.local>
2023-01-14 15:41:19 +0530

    Added all files

---
+++ src/test/java/com/linkedinCourse/demo/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedinCourse.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
